44bc27c8404484ec29837094fd50e4a109da7fb1
Xue Zh <xiaoxueecho@gmail.com>
2023-08-14 08:12:43 +0900

    second try

---
+++ CS61b/intro/loop.java
@@ -0,0 +1,15 @@
+public class loop {
+    // public static void main(String[] args) {
+    //     int x = 0;
+    //     while(x < 10){
+    //         System.out.println(x);
+    //         x ++;
+    //     }
+    // }
+
+    public static void main(String[] args) {
+        for(int x = 0; x < 10; x++){
+            System.out.println(x);
+        }
+    }
+}
